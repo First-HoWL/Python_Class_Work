3419ed57534646e0679af40a876109d61ecce0e3
denis <d24e09n20i09s@ukr.net>
2023-12-18 18:57:49 +0200

    nichogo ne izmeninno

---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+a = float(input('перше число?'))
+b = float(input('друге число?'))
+
+
+print(a * (b / 100))
+"""
+
+a = float(input('ширина прямокутника?'))
+b = float(input('довжина прямокутника?'))
+
+print(a * b)
+
+
+if a < b:
+    print('a < b')
+
+if a > b:
+    print('a > b')
+
+else:
+    print('a = b')
