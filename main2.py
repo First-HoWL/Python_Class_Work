26712535a200e3975a7a98a0ac6881561e799e0c
denis <d24e09n20i09s@ukr.net>
2023-12-18 20:50:36 +0200

    додав це один print

---
+++ main2.py
@@ -0,0 +1,35 @@
+'''
+1
+
+a = int(input('перше число'))
+print( a // 10)
+print(a % 10)
+'''
+'''
+2
+a = int(input('перше число'))
+b = int(a // 100)
+c = int((a // 10) % 10)
+d = int(a % 10)
+print(b)
+print(c)
+print(d)
+print(b + c + d)
+'''
+
+
+'''
+3
+
+a = str(input('перше число'))
+b = str(input('друге число'))
+print(a + b)
+
+'''
+
+'''
+4
+a = int(input('градусів цельсія'))
+print(a * 1.8 + 32)
+
+'''
